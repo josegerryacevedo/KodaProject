a757020cb644d9c9d9c5ac7b87f936121627a5b0
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-09-16 16:33:17 +0800

    install and set up pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default
+    redirect_to(request.referrer || root_path)
+  end
+
+  def pundit_user
+    current_user
+  end
 end
